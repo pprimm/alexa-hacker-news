9e9df34eb56e7e9cece3cce4fde0a7d5620b2220
Pierce Primm <pierce@primm.cc>
2016-11-07 20:12:57 -0600

    Tweaks to syntax

--- apps/my-hacker-news/index.js
+++ apps/my-hacker-news/index.js
@@ -5,27 +5,28 @@ module.change_code = 0;
 
 // Define an alexa-app
 var app = new alexa.app('my-hacker-news');
+
 app.launch(function(req,res) {
-	res.say('Welcome to my hacker news service');
+  res.say('Welcome to my hacker news service');
 });
 
 app.intent('LatestIntent', {
-		"slots":{}
-		,"utterances":['read the latest news']
-	},function(req,res) {
-		res.say('You asked me to read the latest news');
-	}
+    "slots":{},
+    "utterances":['read the latest news']
+  },function(req,res) {
+    res.say('You asked me to read the latest news');
+  }
 );
 
 /*
 https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/speech-synthesis-markup-language-ssml-reference
 */
 app.intent('ItemIndex', {
-		"slots":{"ITEM":"NUMBER"}
-		,"utterances":['read news item number {1-10|ITEM}']
-	},function(req,res) {
-		res.say('You asked me to <w role="ivona:VB">read</w> news item number '+req.slot('ITEM'));
-	}
+    "slots":{"ITEM":"NUMBER"},
+    "utterances":['read news item number {1-10|ITEM}']
+  },function(req,res) {
+    res.say('You asked me to <w role="ivona:VB">read</w> news item number '+req.slot('ITEM'));
+  }
 );
 
 module.exports = app;
