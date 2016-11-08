adc07590c39e498c12c0eefb92c57cb7d49990e6
Pierce Primm <pierce@primm.cc>
2016-11-08 10:58:36 -0600

    Added scraperjs + voice responses to launch intent only.  Tested

--- apps/my-hacker-news/index.js
+++ apps/my-hacker-news/index.js
@@ -1,4 +1,10 @@
 var alexa = require('alexa-app');
+var scraperjs = require('scraperjs');
+
+var latestHNews = {
+  timeStamp: Date.now(),
+  newsItems: {}
+};
 
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 0;
@@ -7,7 +13,28 @@ module.change_code = 0;
 var app = new alexa.app('my-hacker-news');
 
 app.launch(function(req,res) {
-  res.say('Welcome to my hacker news service');
+  res.say('Welcome to my hacker news service.');
+  scraperjs.StaticScraper.create('https://news.ycombinator.com/')
+    .scrape(function($) {
+      return $(".title a").map(function() {
+        return $(this).text();
+      }).get();
+    })
+    .then(function(news) {
+      latestHNews.timeStamp = Date.now();
+      latestHNews.newsItems = news;
+      //console.log(latestHNews.timeStamp);
+      //console.log(latestHNews.newsItems);
+      //res.say('Done retrieveing results.').send();
+      res.say('<p> Number one. ' + latestHNews.newsItems[0] + '.</p>')
+      .say('<p> Number two. ' + latestHNews.newsItems[2] + '.</p>')
+      .say('<p> Number three. ' + latestHNews.newsItems[4] + '.</p>')
+      .say('<p> Number four. ' + latestHNews.newsItems[6] + '.</p>')
+      .say('<p> Number five. ' + latestHNews.newsItems[8] + '.</p>')
+      .send();
+      return true;
+    });
+    return false;
 });
 
 app.intent('LatestIntent', {
